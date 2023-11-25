46bd769f747522f8ec6291f290e260ac9b4ae128
NoTitec <zkxkr0@gmail.com>
2023-11-25 22:09:34 +0900

    Configuration Set

---
+++ Algorithm/Maze/Types.h
@@ -0,0 +1,10 @@
+#pragma once
+using int8 = __int8;
+using int16 = __int16;
+using int32 = __int32;
+using int64 = __int64;
+// alt+마우스 누르면 위아래 드래그 선택가능
+using uint8 = unsigned __int8;
+using uint16 = unsigned __int16;
+using uint32 = unsigned __int32;
+using uint64 = unsigned __int64;
---
+++ Algorithm/Maze/pch.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "Types.h"
+#include<windows.h>
+#include<iostream>
+#include<vector>
+using namespace std;
